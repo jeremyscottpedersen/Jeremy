cc381ac30f2b86121519b1b3fb43f3840d7d6045
Jeremy <jeremy@jspconsulting.com.au>
2024-03-19 12:38:21 +1100

    Task 4,1

---
+++ Github/SIT323/Week 3/logger.js
@@ -0,0 +1,28 @@
+const winston = require('winston');
+
+// Configure the Winston logger.
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.json()
+    ),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.Console({
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            ),
+        }),
+    ],
+});
+
+module.exports = logger;
+
